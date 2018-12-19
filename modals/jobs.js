aec30a5789d48e720d3a8b325ab3d0e5bf746b54
kranthikandi <kranthikumar.kandi@gmail.com>
2018-12-18 21:56:46 -0800

    updated job modal and router

--- modals/jobs.js
+++ modals/jobs.js
@@ -25,6 +25,34 @@ const ContactSchema = mongoose.Schema({
         type:String,
         require:true
     },
+    job_type:{
+        type:String,
+        require:true
+    },
+    notes:{
+        type:String,
+        require:true
+    },
+    weekday:{
+        type:Object,
+        require:true
+    },
+    weekend:{
+        type:Object,
+        require:true 
+    },
+    load:{
+        type:Object,
+        require:true 
+    },
+    ton:{
+        type:Object,
+        require:true
+    },
+    notes:{
+        type:String,
+        require:false
+    },
     start_date:{
         type:String,
         require:true
--- routes/jobs.js
+++ routes/jobs.js
@@ -11,14 +11,20 @@ router.get('/jobs', (req, res, next)=>{
 })
 
 //add contact
-router.post('/job',(req, res, next)=>{
+router.post('/newjob',(req, res, next)=>{
     let newJobs = new Jobs({
         customer_id : req.body.customer_id,
         customer_name : req.body.customer_name,
-        job_name : req.body.job_name,
         job_id : req.body.job_id,
-        job_location : req.body.job_location,
+        job_name : req.body.job_name,
         start_date : req.body.start_date,
+        job_location : req.body.job_location,
+        job_type : req.body.job_type,
+        weekday : req.body.weekday,
+        weekend : req.body.weekend,
+        load : req.body.load,
+        ton : req.body.ton,
+        notes: req.body.notes,
         status : req.body.status
     });
     console.log("req body   ---  "+req.body.job_id);
@@ -48,6 +54,21 @@ router.post('/custjobs',(req, res, next)=>{
     console.log("request for /custjobs"+req.body.customer_id)
 
     Jobs.find({ customer_id: req.body.customer_id }, function (err, result) {
+    if(err){
+        console.log("error for /custjobs"+result)
+        res.json(err);
+   }else{
+        console.log("response body for /custjobs"+result)
+        res.json(result);
+   }
+    })
+})
+
+//get jobs of a customer
+router.post('/jobdetails',(req, res, next)=>{
+    console.log("request for /custjobs"+req.body.customer_id)
+
+    Jobs.find({ job_id: req.body.job_id }, function (err, result) {
     if(err){
         console.log("error for /custjobs"+result)
         res.json(err);
