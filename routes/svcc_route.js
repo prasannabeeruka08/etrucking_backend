bb1a544ad3fe67c109dac54b6f6db7f1f452163d
kranthikandi <kranthikumar.kandi@gmail.com>
2018-12-25 22:25:01 -0800

    fixed svcc

--- routes/svcc_route.js
+++ routes/svcc_route.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Jobs = require('../modals/svcc_modal')
 
+
+
+
 //add contact
-router.post('/svcc',(req, res, next)=>{
+router.post('/svcc18',(req, res, next)=>{
     let newJobs = new Jobs({
         full_name : req.body.fullname,
         email : req.body.email,
         phone : req.body.phone,
-        request : req.body.request,
-    });
+        request : req.body.request
+    }); 
     newJobs.save((err,Driver)=>{
         if(err){
             res.json({msg: 'fail'});
@@ -18,3 +21,5 @@ router.post('/svcc',(req, res, next)=>{
         }
     })
 })
+
+module.exports = router;
