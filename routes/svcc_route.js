54ce0407b999d2e3c6653bd1fb45d27f57f14176
kranthikandi <kranthikumar.kandi@gmail.com>
2018-12-25 22:46:05 -0800

    updated svcc router

--- routes/svcc_route.js
+++ routes/svcc_route.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const Jobs = require('../modals/svcc_modal')
+const svcc = require('../modals/svcc_modal')
 
 
 
 
 //add contact
 router.post('/svcc18',(req, res, next)=>{
-    let newJobs = new Jobs({
+    let newReq = new svcc({
         full_name : req.body.fullname,
         email : req.body.email,
         phone : req.body.phone,
         request : req.body.request
     }); 
-    newJobs.save((err,Driver)=>{
+    newReq.save((err,Driver)=>{
         if(err){
             res.json({msg: 'fail'});
         }else{
