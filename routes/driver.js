22fc13752e812a5424908c08418ea0c04d1c90f6
kranthikandi <kranthikumar.kandi@gmail.com>
2019-01-16 02:34:18 -0800

    updating driver status

--- routes/driver.js
+++ routes/driver.js
@@ -41,7 +41,7 @@ router.delete('/driver/:id',(req, res, next)=>{
 //get driver to dispatch
 router.post('/dispDrivers',(req, res, next)=>{
     console.log("request for /dispDrivers"+req.body.tt)
-    Driver.find({ tt: req.body.tt,status: "available"}, function (err, result) {
+    Driver.find({ tt: req.body.tt,'$or':[{status:'assigned'},{status:'available'}]}, function (err, result) {
     if(err){
         console.log("error for /dispDrivers"+result)
         res.json(err);
@@ -52,4 +52,91 @@ router.post('/dispDrivers',(req, res, next)=>{
  })
 })
 
+router.post('/EDDrivers',(req, res, next)=>{
+    console.log("request for /dispDrivers"+req.body.tt)
+    Driver.find({ tt:'End-Dump','$or':[{status:'assigned'},{status:'available'}]}, function (err, result) {
+    if(err){
+        console.log("error for /EDDrivers"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+   
+ })
+})
+
+router.post('/SDDrivers',(req, res, next)=>{
+    console.log("request for /dispDrivers"+req.body.tt)
+    Driver.find({ tt:'Super-Dump','$or':[{status:'assigned'},{status:'available'}]}, function (err, result) {
+    if(err){
+        console.log("error for /SDDrivers"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+   
+ })
+})
+
+router.post('/DBDrivers',(req, res, next)=>{
+    console.log("request for /dispDrivers"+req.body.tt)
+    Driver.find({ tt:'Double-Bottom','$or':[{status:'assigned'},{status:'available'}]}, function (err, result) {
+    if(err){
+        console.log("error for /DBDrivers"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+   
+ })
+})
+
+router.post('/TenWDrivers',(req, res, next)=>{
+    console.log("request for /dispDrivers"+req.body.tt)
+    Driver.find({ tt:'10-wheeler','$or':[{status:'assigned'},{status:'available'}]}, function (err, result) {
+    if(err){
+        console.log("error for /TenWDrivers"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+   
+ })
+})
+
+router.post('/updateAssDriver',(req, res, next)=>{
+    console.log(" ----   "+req.body.driver)
+    Driver.update({driver_name:req.body.driver}, {$set: {status:"Assigned"}}, function (err, result) {
+    if(err){
+        console.log("error for /updateAssDriver"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+})
+})
+
+router.post('/updateReqDriver',(req, res, next)=>{
+    console.log("request for /updateReqDriver"+req.body.tt)
+    Driver.update({driver_name:req.body.driver_name}, {$set: {atatus:"Requested"}}, function (err, result) {
+    if(err){
+        console.log("error for /updateReqDriver"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+})
+})
+
+router.post('/updateAvaDriver',(req, res, next)=>{
+    console.log("request for /updateAvaDriver"+req.body.tt)
+    Driver.update({driver_name:req.body.driver_name}, {$set: {atatus:"Available"}}, function (err, result) {
+    if(err){
+        console.log("error for /updateAvaDriver"+result)
+        res.json(err);
+   }else{
+        res.json(result);
+   }
+})
+})
 module.exports = router;
